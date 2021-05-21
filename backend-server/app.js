73098940828f17951cb5737eedf4fb5879bc0672
Joel Wembo <joelotepawembo@gmail.com>
2021-05-22 03:40:58 +0800

    Added Pusher websocket realtime module

--- backend-server/app.js
+++ backend-server/app.js
@@ -1,22 +1,44 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import Messages from './dbMessages.js';
+import Pusher from "pusher";
+
 const app = express();
 const port = process.env.PORT || 9000;
 
+
+const pusher = new Pusher({
+  appId: "1207725",
+  key: "d0ce06d1a5b732d08644",
+  secret: "ba5778d9f722b60aa7b7",
+  cluster: "ap1",
+  useTLS: true
+});
+
+pusher.trigger("my-channel", "my-event", {
+  message: "hello world"
+});
+
 const connection_url = 'mongodb://127.0.0.1:27017/whatsapp-db';
 
-try {
+
     mongoose.connect(connection_url, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
-    console.log('connected to the database');
-}
-catch (exception_var) {
-    console.log(exception_var);
-  }
+
+  const db = mongoose.connection;
+    db.once("open", () => {
+      console.log("DB Cnnection");
+
+      const msgCollection = db.collection("messagecontents")
+      const changeStream = msgCollection.watch();
+
+      changeStream.on("change", (change) => {
+          console.log("A change occured", change);
+      })
+  });
 
 app.use(express.json());  
 app.get('/', (req,res) => res.status(200).send('hello word') )
@@ -32,19 +54,7 @@ app.get('/messages/sync', (req, res) => {
   })
 })
 
-const Pusher = require("pusher");
 
-const pusher = new Pusher({
-  appId: "1207725",
-  key: "d0ce06d1a5b732d08644",
-  secret: "ba5778d9f722b60aa7b7",
-  cluster: "ap1",
-  useTLS: true
-});
-
-pusher.trigger("my-channel", "my-event", {
-  message: "hello world"
-});
 
 
 
